3668378581c4dba8da1dcb2087958e1cb0ccdc5f
Minioco7-tech <liamm.oconnor03@gmail.com>
2025-11-25 12:54:31 +0000

    Update model.js

--- js/model.js
+++ js/model.js
@@ -216,7 +216,7 @@ document.addEventListener('DOMContentLoaded', async () => {
               <div class="flex gap-3">
                 
                 <!-- BLUE BULLET -->
-                <span class="w-3 h-3 rounded-full bg-blue-500 mt-1 flex-shrink-0"></span>
+                <span class="w-1 h-1 rounded-full bg-blue-500 mt-1 flex-shrink-0"></span>
         
                 <!-- TEXT BLOCK -->
                 <div class="flex flex-col space-y-1">
