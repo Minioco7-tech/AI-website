00a50a04b06d76181355fd1e3b40609eda2d723c
Minioco7-tech <liamm.oconnor03@gmail.com>
2025-12-11 17:00:17 +0000

    Update model.js

--- js/model.js
+++ js/model.js
@@ -160,6 +160,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         
             <!-- Button -->
             <a href="${model.link}"
+              target="_blank"
               rel="noopener noreferrer"
               class="btn-primary px-4 py-2 text-sm font-medium inline-block self-start">
               Try Model
